0e5d7e38b28a78be7d40e5e3d3be5e5a2aa48dcb
tiesen243 <ttien56906@gmail.com>
2025-03-10 15:25:32 +0700

    chore: minor change

--- COM/app/Form1.Designer.cs
+++ COM/app/Form1.Designer.cs
@@ -28,7 +28,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label_title = new System.Windows.Forms.Label();
             this.groupBox_connect = new System.Windows.Forms.GroupBox();
             this.label_serial_port = new System.Windows.Forms.Label();
@@ -49,19 +50,25 @@
             this.groupBox_controller.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_led)).BeginInit();
             this.SuspendLayout();
-            // 
+            //
             // label_title
-            // 
-            this.label_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            //
+            this.label_title.Font = new System.Drawing.Font(
+                "Microsoft Sans Serif",
+                15.75F,
+                System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point,
+                ((byte)(0))
+            );
             this.label_title.Location = new System.Drawing.Point(16, 16);
             this.label_title.Name = "label_title";
             this.label_title.Size = new System.Drawing.Size(550, 24);
             this.label_title.TabIndex = 6;
             this.label_title.Text = "Communication with Traffic Lights";
             this.label_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
+            //
             // groupBox_connect
-            // 
+            //
             this.groupBox_connect.Controls.Add(this.label_serial_port);
             this.groupBox_connect.Controls.Add(this.comboBox_COMP);
             this.groupBox_connect.Controls.Add(this.textBox_status);
@@ -74,30 +81,32 @@
             this.groupBox_connect.TabIndex = 0;
             this.groupBox_connect.TabStop = false;
             this.groupBox_connect.Text = "Connection";
-            // 
+            //
             // label_serial_port
-            // 
+            //
             this.label_serial_port.Location = new System.Drawing.Point(8, 32);
             this.label_serial_port.Name = "label_serial_port";
             this.label_serial_port.Size = new System.Drawing.Size(84, 20);
             this.label_serial_port.TabIndex = 1;
             this.label_serial_port.Text = "Serial port";
-            // 
+            //
             // comboBox_COMP
-            // 
+            //
             this.comboBox_COMP.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_COMP.FormattingEnabled = true;
             this.comboBox_COMP.Location = new System.Drawing.Point(108, 28);
             this.comboBox_COMP.Name = "comboBox_COMP";
             this.comboBox_COMP.Size = new System.Drawing.Size(128, 28);
             this.comboBox_COMP.TabIndex = 0;
-            this.comboBox_COMP.SelectedIndexChanged += new System.EventHandler(this.comboBox_COMP_SelectedIndexChanged);
-            // 
+            this.comboBox_COMP.SelectedIndexChanged += new System.EventHandler(
+                this.comboBox_COMP_SelectedIndexChanged
+            );
+            //
             // textBox_status
-            // 
-            this.textBox_status.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            //
+            this.textBox_status.BackColor = backgroundColor;
+            this.textBox_status.ForeColor = destructiveColor;
             this.textBox_status.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox_status.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(53)))), ((int)(((byte)(69)))));
             this.textBox_status.Location = new System.Drawing.Point(55, 72);
             this.textBox_status.Name = "textBox_status";
             this.textBox_status.ReadOnly = true;
@@ -105,9 +114,9 @@
             this.textBox_status.TabIndex = 2;
             this.textBox_status.Text = "Disconnected";
             this.textBox_status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
+            //
             // button_connect
-            // 
+            //
             this.button_connect.FlatAppearance.BorderSize = 0;
             this.button_connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_connect.Location = new System.Drawing.Point(12, 108);
@@ -119,9 +128,9 @@
             this.button_connect.BackColor = successColor;
             this.button_connect.ForeColor = primaryForegroundColor;
             this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
-            // 
+            //
             // button_disconnect
-            // 
+            //
             this.button_disconnect.FlatAppearance.BorderSize = 0;
             this.button_disconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_disconnect.Location = new System.Drawing.Point(127, 108);
@@ -133,9 +142,9 @@
             this.button_disconnect.BackColor = destructiveColor;
             this.button_disconnect.ForeColor = primaryForegroundColor;
             this.button_disconnect.Click += new System.EventHandler(this.button_disconnect_Click);
-            // 
+            //
             // groupBox_controller
-            // 
+            //
             this.groupBox_controller.Controls.Add(this.clock);
             this.groupBox_controller.Controls.Add(this.pictureBox_led);
             this.groupBox_controller.Controls.Add(this.button_mode_1);
@@ -149,26 +158,26 @@
             this.groupBox_controller.TabIndex = 0;
             this.groupBox_controller.TabStop = false;
             this.groupBox_controller.Text = "Controller";
-            // 
+            //
             // clock
-            // 
+            //
             this.clock.AutoSize = true;
             this.clock.Location = new System.Drawing.Point(12, 72);
             this.clock.Name = "clock";
             this.clock.Size = new System.Drawing.Size(45, 20);
             this.clock.TabIndex = 9;
             this.clock.Text = "clock";
-            // 
+            //
             // pictureBox_led
-            // 
+            //
             this.pictureBox_led.Location = new System.Drawing.Point(188, 20);
             this.pictureBox_led.Name = "pictureBox_led";
             this.pictureBox_led.Size = new System.Drawing.Size(50, 50);
             this.pictureBox_led.TabIndex = 8;
             this.pictureBox_led.TabStop = false;
-            // 
+            //
             // button_mode_1
-            // 
+            //
             this.button_mode_1.FlatAppearance.BorderSize = 0;
             this.button_mode_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_mode_1.Location = new System.Drawing.Point(16, 110);
@@ -180,9 +189,9 @@
             this.button_mode_1.BackColor = primaryColor;
             this.button_mode_1.ForeColor = primaryForegroundColor;
             this.button_mode_1.Click += new System.EventHandler(this.button_mode_1_Click);
-            // 
+            //
             // button_mode_2
-            // 
+            //
             this.button_mode_2.FlatAppearance.BorderSize = 0;
             this.button_mode_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_mode_2.Location = new System.Drawing.Point(90, 110);
@@ -194,9 +203,9 @@
             this.button_mode_2.BackColor = primaryColor;
             this.button_mode_2.ForeColor = primaryForegroundColor;
             this.button_mode_2.Click += new System.EventHandler(this.button_mode_2_Click);
-            // 
+            //
             // checkBox_control
-            // 
+            //
             this.checkBox_control.AutoSize = true;
             this.checkBox_control.Location = new System.Drawing.Point(16, 32);
             this.checkBox_control.Name = "checkBox_control";
@@ -204,10 +213,12 @@
             this.checkBox_control.TabIndex = 5;
             this.checkBox_control.Text = "No signal";
             this.checkBox_control.UseVisualStyleBackColor = true;
-            this.checkBox_control.CheckedChanged += new System.EventHandler(this.checkBox_control_CheckedChanged);
-            // 
+            this.checkBox_control.CheckedChanged += new System.EventHandler(
+                this.checkBox_control_CheckedChanged
+            );
+            //
             // button_mode_3
-            // 
+            //
             this.button_mode_3.FlatAppearance.BorderSize = 0;
             this.button_mode_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_mode_3.Location = new System.Drawing.Point(164, 110);
@@ -219,27 +230,35 @@
             this.button_mode_3.BackColor = primaryColor;
             this.button_mode_3.ForeColor = primaryForegroundColor;
             this.button_mode_3.Click += new System.EventHandler(this.button_mode_3_Click);
-            // 
+            //
             // serialPort
-            // 
+            //
             this.serialPort.PortName = "COM2";
-            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
-            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(
+                this.serialPort_DataReceived
+            );
+            //
             // timer
-            // 
+            //
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
-            // 
+            //
             // Form1
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.BackColor = backgroundColor;
+            this.ForeColor = foregroundColor;
             this.ClientSize = new System.Drawing.Size(550, 236);
             this.Controls.Add(this.label_title);
             this.Controls.Add(this.groupBox_controller);
             this.Controls.Add(this.groupBox_connect);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Font = new System.Drawing.Font(
+                "Microsoft Sans Serif",
+                12F,
+                System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point,
+                ((byte)(0))
+            );
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -247,7 +266,9 @@
             this.Name = "Form1";
             this.Text = "Communication with Traffic Lights";
             this.TopMost = true;
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(
+                this.Form1_FormClosing
+            );
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox_connect.ResumeLayout(false);
             this.groupBox_connect.PerformLayout();
@@ -255,7 +276,6 @@
             this.groupBox_controller.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_led)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         #endregion
